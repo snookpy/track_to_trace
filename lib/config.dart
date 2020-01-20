50623111a7e7e5b78c8d84500b982c8f0abd534f
Nook <nook@Nooks-MacBook-Pro.local>
2020-01-21 00:30:01 +0700

    fix static ApiKey

--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,3 @@
 class Config {
-  final String thaiPostApiKey = "test";
+  static final String thaiPostApiKey = "BeV4EvNiUISfC-B^BYZOM6X0IEGTNjBLH^UdZmR0BUPlE8F@P\$UHZXJ1BmG0O=XqO|O1N\$X*CiReLnY3UiVLP6RkV9S*GtRxFsBq";
 }
